cb82b5ef2c13b3f79e04d1ae88d8d978551bae61
nurbek20 <nurbek19957072@gmail.com>
2022-11-16 20:25:52 +0600

    Counter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Counter from './Components/Counter';
+
+const App = () => {
+  return (
+    <>
+      <Counter />
+
+    </>
+  );
+}
+
+export default App;
